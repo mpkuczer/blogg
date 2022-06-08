14dfcb8b35c6892854356d1705e60bc012fc1a91
mkzr <mpkuczer@gmail.com>
2022-06-08 20:09:02 +0200

    Added devise and learned a bit about migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_08_112459) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_180135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,12 +21,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_112459) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "username"
-    t.text "email"
-    t.text "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
