4bc9a261988018c65dc2b699ffd7111b9bdd9341
mpkuczer <mpkuczer@gmail.com>
2022-06-10 12:37:09 +0200

    remove obsolete migrations

--- db/migrate/20220608112254_change_tweeets_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTweeetsToPosts < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :tweeets, :posts
-  end
-end
--- db/migrate/20220608112459_change_tweeet_to_post.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTweeetToPost < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :posts, :tweeet, :post
-  end
-end
