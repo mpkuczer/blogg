3e570993d3f33180dd6db20e0b84526713e7bfbb
mpkuczer <mpkuczer@gmail.com>
2022-06-10 14:00:27 +0200

    Added logic to views

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
+
+module ActiveRecord
+  class Base
+    def self.random
+      if (c = count) != 0
+        find(rand(c)+1)
+      end
+    end
+  end
+end
